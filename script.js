dccec201d5868a2f952c7189d94bb181c28bf3a9
cproutt <thechrisproutt@gmail.com>
2020-11-04 19:16:17 -0800

    Update with some Script

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mobile Menu button
+const showMobileMenu() {
+  var x = document.getElementById("mobile");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
