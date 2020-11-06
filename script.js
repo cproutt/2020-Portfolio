b91ec8a5723ee6d03e8efd885707d00b6fc2c39a
cproutt <thechrisproutt@gmail.com>
2020-11-06 09:36:58 -0800

    Updated Javascript

--- script.js
+++ script.js
@@ -8,9 +8,19 @@ const showMobileMenu = () => {
   }
 }
 
-// Toggle Skills Section
-$(document).ready(function(){
-  $("#skills-cta").click(function(){
-    $("#skills").toggle(900);
-  });
-});
+//Scroll down Sticky Nav
+const shotStickyNav = () => {
+  //Check if screen size is medium or wider
+  //Get the top positon of window
+  //If top position is greater than 250px, toggle sticky nav;
+
+  if(window.innerHeight >= 768){
+    let topOfScreen = window.pageYOffset;
+    const fixedNav = document.getElementById('sticky-nav');
+    if(topOfScreen > 300){
+        fixedNav.style.display = "fixed";
+    } else {
+        fixedNav.style.display = "none"
+    }
+  }
+}
